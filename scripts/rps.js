e75bfa56a1f5b98a205aa33f3a7b65ecc7158f05
Francisjung17 <78046912+Francisjung17@users.noreply.github.com>
2023-06-21 14:38:32 -0500

    stashing changes

--- scripts/rps.js
+++ scripts/rps.js
@@ -7,8 +7,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-let player1 = 0;
-let player2 = 0;
+let player1 =0;
+let player2 =1;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/rps.html');
@@ -17,9 +17,15 @@ io.on('connection', (socket) =>{
     console.log('a user connected!');
      socket.on('selection', (selection) => {
         player1 = selection;
-    console.log("player has selected: "+selection);
-    
+    console.log("player 1 has selected: "+selection);
+    if(player1){
+        player2 = selection;
+        console.log("player 2 has selected: "+selection);
+    }
   });
+  if(player1 && player2){
+    game(player1, player2);
+  }
 });
 
 server.listen(3002, () =>{
@@ -28,10 +34,13 @@ server.listen(3002, () =>{
 
 function game(choice1, choice2){
     if(choice1==choice2){
+        console.log('Tie');
         return 0;
     }else if((choice1+1) % 3 == choice2 % 3){
+        console.log('Player 1 wins');
         return 1;
     }else{
+        console.log('Player 2 wins');
         return 2;
     }
 }
